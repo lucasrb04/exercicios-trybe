c972d12c4cc7996b03725767b6fec57453918b7f
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-01 09:20:46 -0300

    exercicio 6

---
+++ exercises/fundamentosWeb/bloco-4/dia-1/xadrezMovimento.js
@@ -0,0 +1,25 @@
+let chessPiece = "bishop";
+chesspiece = chessPiece.toLowerCase
+switch (chessPiece) {
+  case "bishop":
+    console.log("Bishop -> Diagonals")
+    break;
+  case "king":
+    console.log("King -> 1 square for all directions")
+    break;
+  case "queen":
+    console.log("Queen -> Diagonals, horizontal and vertical")
+    break;
+  case "pawn":
+    console.log("Pawn -> Advancing 1 square, but the first time, it has the option of advancing 2 squares")
+    break;
+  case "rook":
+    console.log("Rook -> Horizontal and vertical")
+    break;
+  case "knight":
+    console.log("Knight -> Shape of an L and jump over other pieces")
+    break;
+  default:
+    console.log("Invalid piece.")
+    break;
+}

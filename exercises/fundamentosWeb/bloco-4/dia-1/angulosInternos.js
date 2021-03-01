a76bed8f74c154ecadd69b5b26c50780e91e4d17
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-01 08:43:16 -0300

    melhorando descriçao do exercicio 5.

--- exercises/fundamentosWeb/bloco-4/dia-1/angulosInternos.js
+++ exercises/fundamentosWeb/bloco-4/dia-1/angulosInternos.js
@@ -1,9 +1,11 @@
 let angulo1 = 60;
 let angulo2 = 60;
 let angulo3 = 60;
+let somaAngulos = angulo1 + angulo2 + angulo3;
+//testando se algum dos 3 ângulos são negativos.
 if (angulo1 < 0 || angulo2 < 0 || angulo3 < 0){
   console.log("Erro: 'foram digitados ângulos negativos!!'");
-} else if (angulo1 + angulo2 + angulo3 == 180) {
+} else if (somaAngulos == 180) {
   console.log(true);
 } else { 
   console.log(false);

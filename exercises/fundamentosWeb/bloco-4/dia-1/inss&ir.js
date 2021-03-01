8b57e1df249fa82e0e10610a326873329e1cc9c1
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-01 12:13:13 -0300

    terminando exercicio 11, calculando a aliquota de IR e o salario liquido.

--- exercises/fundamentosWeb/bloco-4/dia-1/inss&ir.js
+++ exercises/fundamentosWeb/bloco-4/dia-1/inss&ir.js
@@ -1,24 +1,28 @@
 let aliquotaInss;
-let aliquitaIr;
+let aliquotaIr;
 let salarioBase;
 let salarioLiquido;
-let salarioBruto = 1500;
+let salarioBruto = 2000;
 if (salarioBruto > 5189.82) {
   aliquotaInss = 570.88;
 } else if (salarioBruto >= 2594.93) {
   aliquotaInss = 0.11 * salarioBruto;
 } else if (salarioBruto >= 1556.95) {
   aliquotaInss = 0.09 * salarioBruto;
-} else if (salarioBruto < 1556.95) {
+} else {
   aliquotaInss = 0.08 * salarioBruto;
 }
 salarioBase = salarioBruto - aliquotaInss;
-
-
-
-
-// Até R$ 1.903,98: isento de imposto de renda
-// De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
-// De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
-// De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
-// Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.
+if (salarioBase > 4664.68) {
+  aliquotaIr = (salarioBase * 0.275) - 869.36;
+} else if (salarioBase > 3751.06) {
+  aliquotaIr = (salarioBase * 0.225) - 636.13;
+} else if (salarioBase > 2826.66) {
+  aliquotaIr = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase > 1903.99) {
+  aliquotaIr = (salarioBase * 0.075) - 142.80;
+} else {
+  aliquotaIr = 0;
+}
+salarioLiquido = salarioBase - aliquotaIr;
+console.log(salarioLiquido);

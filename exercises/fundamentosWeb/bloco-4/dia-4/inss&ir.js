cc08997ec7d065b5e2b7f3cc8e35979fddf8cfed
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-07 12:58:43 -0300

    copiando exercicios do 4.1 para 4.4 e realizando  angulosInternos.js com functions.

---
+++ exercises/fundamentosWeb/bloco-4/dia-4/inss&ir.js
@@ -0,0 +1,31 @@
+let aliquotaInss;
+let aliquotaIr;
+
+let salarioBruto = 3000;
+
+if (salarioBruto > 5189.82) {
+  aliquotaInss = 570.88;
+} else if (salarioBruto >= 2594.93) {
+  aliquotaInss = 0.11 * salarioBruto;
+} else if (salarioBruto >= 1556.95) {
+  aliquotaInss = 0.09 * salarioBruto;
+} else {
+  aliquotaInss = 0.08 * salarioBruto;
+};
+
+let salarioBase = salarioBruto - aliquotaInss;
+
+if (salarioBase > 4664.68) {
+  aliquotaIr = (salarioBase * 0.275) - 869.36;
+} else if (salarioBase > 3751.06) {
+  aliquotaIr = (salarioBase * 0.225) - 636.13;
+} else if (salarioBase > 2826.66) {
+  aliquotaIr = (salarioBase * 0.15) - 354.80;
+} else if (salarioBase > 1903.99) {
+  aliquotaIr = (salarioBase * 0.075) - 142.80;
+} else {
+  aliquotaIr = 0;
+};
+
+let salarioLiquido = salarioBase - aliquotaIr;
+console.log("O salário é: " + salarioLiquido + " reais");
---
+++ exercises/fundamentosWeb/bloco-4/dia-4/isEven.js
@@ -0,0 +1,9 @@
+let number1 = 1;
+let number2 = 1;
+let number3 = -1;
+let isEven = number1 % 2 == 0 || number2 % 2 == 0 || number3 % 2 == 0;
+if (isEven) {
+  console.log(true)
+} else {
+  console.log(false)
+}

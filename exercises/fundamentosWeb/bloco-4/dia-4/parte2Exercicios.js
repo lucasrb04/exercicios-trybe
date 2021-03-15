b96930e0046ce99159f10e5bbdabe06c35a5231a
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-15 00:49:59 -0300

    realizando exercicios parte 2 e bonus'

--- exercises/fundamentosWeb/bloco-4/dia-4/parte2Exercicios.js
+++ exercises/fundamentosWeb/bloco-4/dia-4/parte2Exercicios.js
@@ -19,11 +19,11 @@ function indiceMaiorValor(array) {
       max = array[index]
       indice = index
     }
-    
+
   }
   return indice;
 }
-console.log(indiceMaiorValor([1,2,3,101,5,6,100,2]));
+console.log(indiceMaiorValor([1, 2, 3, 101, 5, 6, 100, 2]));
 
 // 3
 
@@ -35,40 +35,40 @@ function menorValor(array) {
       min = array[index]
       indice = index
     }
-    
+
   }
   return indice;
 }
-console.log(menorValor([-10,2,3,101,-5,6,-100,2]));
+console.log(menorValor([-10, 2, 3, 101, -5, 6, -100, 2]));
 
 // 4
 
 function biggerName(arrayOfNames) {
   let biggerName = arrayOfNames[0];
-   for (let index = 0; index < arrayOfNames.length; index++) {
+  for (let index = 0; index < arrayOfNames.length; index++) {
     if (biggerName.length < arrayOfNames[index].length) {
       biggerName = arrayOfNames[index]
     }
-    
+
   }
   return biggerName;
 }
 console.log(biggerName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
 // 5
-function moreRepeatedNumber (arrayNumbers) {
-let arrayLength = [];
-for (let index in arrayNumbers) {
-  let count = 0;
-  for (let index2 in arrayNumbers) {
-    if (arrayNumbers[index] === arrayNumbers[index2]) { 
-      count += 1; 
+function moreRepeatedNumber(arrayNumbers) {
+  let arrayLength = [];
+  for (let index in arrayNumbers) {
+    let count = 0;
+    for (let index2 in arrayNumbers) {
+      if (arrayNumbers[index] === arrayNumbers[index2]) {
+        count += 1;
+      }
     }
+    arrayLength.push(count)
   }
-  arrayLength.push(count)
+  return arrayNumbers[indiceMaiorValor(arrayLength)]
 }
-return arrayNumbers[indiceMaiorValor(arrayLength)]
-}
-console.log(moreRepeatedNumber([1,2,3,4,5,6,45,3]));
+console.log(moreRepeatedNumber([1, 2, 3, 4, 5, 6, 45, 3]));
 // 6
 function sumNumber(number) {
   let sum = 0;
@@ -89,4 +89,35 @@ function verificaFimPalabra(stringWord, stringEnding) {
     return false;
   }
 }
-console.log(verificaFimPalabra("Hello world!","world!"));
+console.log(verificaFimPalabra("Hello world!", "world!"));
+
+// desafio code wars
+function sum(number1, number2) {
+  number1 = number1.toString().split('').reverse().join('');
+  number2 = number2.toString().split('').reverse().join('');
+  let result = '';
+  if (number1.length < number2.length) {
+    [number1, number2] = [number2, number1]
+  }
+    for (let index = 0; index < number1.length; index++) {
+      let int1 = parseInt(number1[index]);
+      let int2 = parseInt(number2[index] || 0);
+      result = (int1 + int2).toString() + result
+    }
+
+    return (parseInt(result))
+  }
+
+console.log(sum(123, 2188));
+    // for(let i = number1.length; i > 0; i -= 1) {
+    //   soma = parseInt(number1[i - 1]) + parseInt(number2[i - 1]);
+    //   console.log(soma);
+    //   resultArray.unshift(soma.toString());
+    // }
+    // for(key in resultArray) {
+    //   result = result + resultArray[key]
+    // }
+        // }
+    // while (number2.length < number1.length) {
+    //   number2.unshift(0);
+    // }

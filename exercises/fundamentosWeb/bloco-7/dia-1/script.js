9e34328dc41d1cdf800a6b5519a4f22b7a9d0ede
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-23 21:09:48 -0300

    adicionando exercicio parte 1

--- exercises/fundamentosWeb/bloco-7/dia-1/script.js
+++ exercises/fundamentosWeb/bloco-7/dia-1/script.js
@@ -1,13 +1,17 @@
-function testingScope(escopo) {
+const testingScope = escopo => {
   if (escopo === true) {
     var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`
     console.log(ifScope);
   } else {
     var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 }
 
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+
+console.log(`Os números ${oddsAndEvens.sort((a,b) =>  a - b )} se encontram ordenados de forma crescente` );

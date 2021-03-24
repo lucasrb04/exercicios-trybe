93d7f2052d727c793dc5c155dbedf6eb4a25aa19
Lucas Ribeiro <lucasrb04@gmail.com>
2021-03-24 18:46:40 -0300

    atualizando informaçoes

--- exercises/fundamentosWeb/bloco-7/dia-1/script.js
+++ exercises/fundamentosWeb/bloco-7/dia-1/script.js
@@ -15,3 +15,8 @@ const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 
 console.log(`Os números ${oddsAndEvens.sort((a,b) =>  a - b )} se encontram ordenados de forma crescente` );
+
+// const fatorial = (n === 0) ? n -= 1 
+
+const factorial = number => number > 1 ? number * factorial(number - 1) : 1
+console.log(factorial(5))

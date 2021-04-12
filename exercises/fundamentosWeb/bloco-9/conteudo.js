f2b874fba0d8b9818de8da89764abed8f47a9b13
Lucas Ribeiro <lucasrb04@gmail.com>
2021-04-12 20:23:57 -0300

    adicionando exercicios do conteudo e iniciando exercicio 1 da seçao de exercicios

---
+++ exercises/fundamentosWeb/bloco-9/conteudo.js
@@ -0,0 +1,22 @@
+setTimeout(() => {
+  console.log('Comprar parafusos') // Comprar parafusos
+  console.log('Adicionar ao estoque') // Adicionar ao estoque
+}, 2000);
+
+console.log('1 - Receber roda'); // 1 - Receber roda
+console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
+console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
+
+// =========================================================
+
+const assert = require('assert');
+
+const pushNumber = (list, number) => list.push(number);
+
+let numbers = [];
+
+pushNumber(numbers, 1);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
+
+assert.deepStrictEqual(numbers, [1, 2, 3]);
